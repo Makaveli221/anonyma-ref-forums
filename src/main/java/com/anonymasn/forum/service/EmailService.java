757058c66da3e1dbd0c96722e2e80c0b29e7ce36
Ousmane Ndiaye <ousmanendiaye352@gmail.com>
2020-04-19 13:31:28 +0000

    add config aumonemailer

--- src/main/java/com/anonymasn/forum/service/EmailService.java
+++ src/main/java/com/anonymasn/forum/service/EmailService.java
@@ -2,4 +2,6 @@ package com.anonymasn.forum.service;
 
 public interface EmailService {
   public void sendSimpleMessage(String to, String subject, String text);
+
+  public void sendMessageWithAttachment(String to, String subject, String text, String pathToAttachment);
 }
